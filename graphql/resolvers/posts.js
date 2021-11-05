dc3fd86da3a884f8780cf249a1a380c3a6e9fa1f
Amro Saad <amro.gamal2000@gmail.com>
2021-11-05 12:46:48 +0200

    Create and Delete posts

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,10 +1,13 @@
+const { AuthenticationError } = require("apollo-server");
+
 const Post = require("../../models/Post");
+const checkAuth = require("../../util/check-auth");
 
 module.exports = {
   Query: {
     async getPosts() {
       try {
-        const posts = await Post.find();
+        const posts = await Post.find().sort({ createdAt: "desc" });
         return posts;
       } catch (err) {
         throw new Error(err);
@@ -17,6 +20,39 @@ module.exports = {
           return post;
         } else {
           throw new Error("This post does not exist");
+        }
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+  },
+  Mutation: {
+    async createPost(_, { body }, context) {
+      const user = checkAuth(context);
+
+      // We have a user here
+
+      const newPost = new Post({
+        body,
+        user: user.indexOf,
+        username: user.username,
+        createdAt: new Date().toISOString(),
+      });
+
+      const post = await newPost.save();
+
+      return post;
+    },
+    async deletePost(_, { postId }, context) {
+      const user = checkAuth(context);
+
+      try {
+        const post = await Post.findById(postId);
+        if (user.username === post.username) {
+          await post.delete();
+          return "Post deleted successfully";
+        } else {
+          throw new AuthenticationError("This user is not allowed to delete this post!");
         }
       } catch (err) {
         throw new Error(err);
